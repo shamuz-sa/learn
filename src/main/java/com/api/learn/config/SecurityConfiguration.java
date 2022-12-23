bc9d39596cacb25452baeec29dcd2bbf993c6b22
geoffreylgv <geoffreylogovi2@gmail.com>
2022-12-23 18:53:05 +0000

    updated

--- src/main/java/com/api/learn/config/SecurityConfiguration.java
+++ src/main/java/com/api/learn/config/SecurityConfiguration.java
@@ -8,5 +8,5 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-    
+
 }
